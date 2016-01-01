2162b1e381e050a148b6601a35798d9149768e9f
Adel Qalieh <aqalieh95@gmail.com>
2016-01-01 01:09:24 -0500

    Implement memoized fibonacci

--- client/utils.js
+++ client/utils.js
@@ -2,10 +2,26 @@ Template.registerHelper('percentage', function () {
   return 100 * this.time / this.goal;
 });
 
-var fibonacci = function (n) {
-  if (n === 0 || n === 1) {
-    return n;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
-};
+var fibonacci = (function () {
+  var memo = {};
+
+  var fib = function (n) {
+    var result;
+
+    if (n in memo) {
+      result = memo[n];
+    } else {
+      if (n === 0 || n === 1) {
+        result = n;
+      } else {
+        result = fib(n - 1) + fib(n - 2);
+      }
+
+      memo[n] = result;
+    }
+
+    return result;
+  };
+
+  return fib;
+})();
