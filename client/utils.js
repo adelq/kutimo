07f60cff4d08195e866af52103bb6a8ccca0ef5c
Adel Qalieh <aqalieh95@gmail.com>
2016-01-01 02:00:49 -0500

    Round percentage done

--- client/utils.js
+++ client/utils.js
@@ -2,6 +2,10 @@ Template.registerHelper('percentage', function () {
   return 100 * this.time / this.goal;
 });
 
+Template.registerHelper('round', function (n) {
+  return Math.round(n);
+});
+
 var fibonacci = (function () {
   var memo = {};
 
