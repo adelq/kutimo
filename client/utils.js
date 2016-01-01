3bb3c79544f5c3b5fc3d3674ddf289aeb8042569
Adel Qalieh <aqalieh95@gmail.com>
2016-01-01 01:06:36 -0500

    Add fibonacci implementation

--- client/utils.js
+++ client/utils.js
@@ -1,3 +1,11 @@
 Template.registerHelper('percentage', function () {
   return 100 * this.time / this.goal;
 });
+
+var fibonacci = function (n) {
+  if (n === 0 || n === 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
